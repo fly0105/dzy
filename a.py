f42c27fa8a450484320b2db9285d5b5881f1a016
fly0105 <2960635580@qq.com>
2019-09-28 10:28:19 -0400

    a1

--- a.py
+++ a.py
@@ -1,3 +1,15 @@
-print('hello');
-print('a.py');
-123
+#coding=utf-8
+import requests
+import json
+
+post_url="https://fanyi.baidu.com/langdetect"
+post_data={
+        "query":"昨天",
+        
+}
+header={
+"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"        
+}
+res=requests.post(post_url,headers=header,data=post_data)
+dict=json.loads(res.content.decode());
+print(dict)
